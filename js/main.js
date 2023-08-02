17c0b16b60c7ddf993102166d063d21097fd5ae7
Vendaarlia <94086273+Vendaarlia@users.noreply.github.com>
2023-08-02 14:45:55 +0700

    custome scrollbar di index

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-const text = new SplitType('.text')
+let links = document.querySelectorAll ('.selected-work__item');
+let linksImages = document.querySelectorAll ('.hover-reveal__img');
 
-gsap.to('.char', {
-    y: 0,
-    stagger: 0.05,
-    delay: 0.3,
-    duration: 3,
-})
+links.forEach((link, idx) => {
+    link.addEventListener('mousemove', (e) =>{
+        link.children[1].style.opacity = 1;
+        link.children[0].style.zIndex = 3;
+        link.children[1].style.transform = `translate(${e.clientX - 200}px, -${e.clientY / 3}px) rotate(${e.clientX / 50}deg)`;
+        linksImages[idx].style.transform = 'scale(1, 1)';
+        link.style.zIndex = 2;
+    })
+
+    link.addEventListener('mouseleave', (e) => {
+        link.children[1].style.opacity = 0;
+        link.children[1].style.transform = `translate(${-e.clientX}px, -300px)`
+        linksImages[idx].style.transform = 'scale(0.8, 0.8)';
+        link.style.zIndex = 0
+    })
+});
---
+++ js/prefilledLink.js
@@ -0,0 +1,5 @@
+  let name = "";
+  let email = "";
+  let message = "";
+			
+  $: prefilledLink = `https://docs.google.com/forms/d/e/1FAIpQLScXmXsv66bbA1QuwdJMBrJpQbTAcg5LxDicbs17cJhypOhGqw/viewform?usp=pp_url&entry.2005620554=${name}&entry.1045781291=${email}&entry.839337160=${message}&submit=Submit`;
