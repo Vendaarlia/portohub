4f93dfa33ce523798807205aa705c17e7ff7674f
Vendaarlia <94086273+Vendaarlia@users.noreply.github.com>
2023-08-02 14:48:20 +0700

    Delete _pgbackup directory

--- _pgbackup/js/main_1677281906.js
+++
@@ -1,8 +0,0 @@
-const text = new SplitType('.text')
-
-tween('.char', {
-    y: 0,
-    stagger: 0.05,
-    delay: 0.3,
-    duration: .3,
-})
--- _pgbackup/js/main_1677282669.js
+++
@@ -1,8 +0,0 @@
-const text = new SplitType('.text')
-
-gsap.to('.word', {
-    y: 0,
-    stagger: 0.05,
-    delay: 0.3,
-    duration: 0.3,
-})
